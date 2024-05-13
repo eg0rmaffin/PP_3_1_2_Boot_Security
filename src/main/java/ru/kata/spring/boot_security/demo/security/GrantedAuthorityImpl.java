70aa4a333be5bf2f3075e6384f70e5e549c2f54a
Egor Markin <eg0rmaffin@gmail.com>
2024-05-13 22:14:29 +0300

    solid issues reworked

---
+++ src/main/java/ru/kata/spring/boot_security/demo/security/GrantedAuthorityImpl.java
@@ -0,0 +1,17 @@
+package ru.kata.spring.boot_security.demo.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class GrantedAuthorityImpl implements GrantedAuthority {
+
+    private String name;
+
+    public GrantedAuthorityImpl(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name;
+    }
+}
