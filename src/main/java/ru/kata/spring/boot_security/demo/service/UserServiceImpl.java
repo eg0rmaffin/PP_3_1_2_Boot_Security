48a2cdd6d19e561cb14b0b6cb72fc232dad7fe70
Egor Markin <eg0rmaffin@gmail.com>
2024-05-22 12:11:39 +0300

    added password form

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.service;
 
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
@@ -18,10 +19,13 @@ public class UserServiceImpl implements UserService {
     private final UserDao userDao;
     private final RoleDao roleDao;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserServiceImpl(UserDao userDao, RoleDao roleDao) {
+    public UserServiceImpl(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
         this.roleDao = roleDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -52,6 +56,7 @@ public class UserServiceImpl implements UserService {
                 .map(id -> roleDao.getRoleById(id)) // Предполагается, что у тебя есть такой метод в RoleDao
                 .collect(Collectors.toSet());
         user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDao.addUser(user);
     }
 
@@ -64,6 +69,7 @@ public class UserServiceImpl implements UserService {
             existingUser.setLastName(updatedUser.getLastName());
             existingUser.setEmail(updatedUser.getEmail());
             existingUser.setRoles(updatedUser.getRoles());
+            existingUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
             userDao.updateUser(existingUser);
         }
     }
