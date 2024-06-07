35a2b22a87e45b0ec2bc90ce49564762d3e5b7c8
Egor Markin <eg0rmaffin@gmail.com>
2024-06-07 17:10:16 +0300

    added spring cloud compatibility

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -3,9 +3,11 @@ package ru.kata.spring.boot_security.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "ru.kata.spring.boot_security.demo.model" })
+@EnableEurekaClient
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
